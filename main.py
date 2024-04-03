9c71f86e6059da30bc292805a24a8c7aae60ff83
Anurajrajsa <165904730+Anurajrajsa@users.noreply.github.com>
2024-04-03 17:29:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,11 +62,11 @@ def progress(current, total, message, type):
 		fileup.write(f"{current * 100 / total:.1f}%")
 
 
-# start command
+# rajsa command
 @bot.on_message(filters.command(["start"]))
-def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+def send_rajsa(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/rajsarajmovie")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
